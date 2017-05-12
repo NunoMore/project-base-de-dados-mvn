1b20c0530a8088204d4f5304407d89119c703f67
Stiffrain <nunomoreira16@gmail.com>
2017-05-12 18:17:54 +0100

    Add files via upload

--- src/main/java/io/altar/jseproject/stateinterface/StateMainMenu.java
+++ src/main/java/io/altar/jseproject/stateinterface/StateMainMenu.java
@@ -4,53 +4,52 @@ import io.altar.jseproject.model.Entity;
 
 public class StateMainMenu extends State{
 	
+	private int[] options = {1,2,3};
+	
 	protected void on() {
 
 		String entidade = "";
-		
-		Boolean run = true;
-		while( run ){
 
-			System.out.println("\nPor favor selecione uma das seguintes opcoes: "
-					+ "\n1) Listar produtos"
-					+ "\n2) Listar prateleiras"
-					+ "\n3) Sair");
-			
-			int option = Entity.checkInputInt();
+		System.out.println("\nPor favor selecione uma das seguintes opcoes: "
+				+ "\n1) Listar produtos"
+				+ "\n2) Listar prateleiras"
+				+ "\n3) Sair");
 		
-			switch (option){
-			
-				case 1: //Listar produtos
+		int option = checkOptions(options);
+	
+		switch (option){
+		
+			case 1: //Listar produtos
+				
+				entidade = "produto";
+				productRepository.has(entidade);
+				productRepository.printList(entidade);
+				
+				FSM.changeState(option);
+				subMenu(productRepository, entidade);  
+				break;
 					
-					entidade = "produto";
-					productRepository.has(entidade);
-					productRepository.printList(entidade);
-					
-					subMenu(productRepository, entidade);  
-					break;
-						
-				case 2: //Listar prateleiras
+			case 2: //Listar prateleiras
 
-					entidade = "prateleira";
-					shelfRepository.has(entidade);
-					shelfRepository.printList(entidade);
+				entidade = "prateleira";
+				shelfRepository.has(entidade);
+				shelfRepository.printList(entidade);
+				
+				subMenu(shelfRepository, entidade); 
+				break;
+				
+			case 3: //Sair
+				
+				System.out.println("\nGood bye!");
+				run = false;
+				break;
 					
-					subMenu(shelfRepository, entidade); 
-					break;
-					
-				case 3: //Sair
-					
-					System.out.println("\nGood bye!");
-					run = false;
-					break;
-						
-				default: 
-					System.out.println("\nInvalid input."
-										+ "\n\nChoose a valid option!");
-					break; 
-			}
+			default: 
+				System.out.println("\nInvalid input."
+									+ "\n\nChoose a valid option!");
+				break; 
 		}
-    }
+	}
 
 	protected void off() {
         System.out.println("error");
